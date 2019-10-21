98d7ce6957dacbb39316202ce33a2ee86f86a838
Krasen Ivanov <krasen007@gmail.com>
2019-10-21 12:29:42 +0300

    Move folders around

--- MoneyExperiment/Program.cs
+++ MoneyExperiment/Program.cs
@@ -3,7 +3,6 @@
 namespace MoneyExperiment
 {
     using System;
-    using System.Security.Principal;
 
     public static class Program
     {
@@ -11,24 +10,7 @@ namespace MoneyExperiment
         {
             Console.Title = "Money Experiment " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 
-            #if RELEASE
-            bool isElevated;
-            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
-            {
-                WindowsPrincipal principal = new WindowsPrincipal(identity);
-                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
-            }
-
-            if (isElevated)
-            {
-                _ = new Begin();
-            }
-            else
-            {
-                Console.WriteLine("You need admin privileges to run this app.\nPress any key to exit...");
-                Console.ReadKey();
-            }
-            #endif
+            _ = new Begin();
         }
     }
 }
