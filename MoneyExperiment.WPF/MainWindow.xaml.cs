064451b4433275442ade5d62011d64435d676b95
Krasen <krasen007@gmail.com>
2020-02-02 11:38:29 +0200

    Update MainWindows.xaml

--- MoneyExperiment.WPF/MainWindow.xaml.cs
+++ MoneyExperiment.WPF/MainWindow.xaml.cs
@@ -11,5 +11,25 @@ namespace MoneyExperiment.WPF
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // TO be implemented.
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // TO be implemented.
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // TO be implemented.
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // TO be implemented.
+        }
     }
 }
